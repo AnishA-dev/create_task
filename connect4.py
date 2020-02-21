c5a198b9a3649110e4bdce86929c69b8206a0261
AnishA-dev <58665659+AnishA-dev@users.noreply.github.com>
2020-02-21 10:13:48 -0500

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -4,10 +4,14 @@ from colors import *
 
 pygame.init()
 
-size = (350, 300)
+size = (310, 310)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Connect 4")
 finished = False
+
+slots = []
+
+x_list = [5, 55, 105, 155, 205, 255, 305]
 
 def draw_background():
   for i in range 42:
