9a46e8612dd458f2b838368462b54ee081e13183
AnishA-dev <58665659+AnishA-dev@users.noreply.github.com>
2020-02-21 10:10:46 -0500

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -7,3 +7,8 @@ pygame.init()
 size = (350, 300)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Connect 4")
+finished = False
+
+def draw_background():
+  for i in range 42:
+    
